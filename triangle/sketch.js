cec4154d6c6e8104c1097aca0b471648d963237f
TheBlackGuy23 <lyndonlivingston@gmail.com>
2024-11-25 19:23:55 -0800

    1st push

---
+++ triangle/sketch.js
@@ -0,0 +1,172 @@
+function setup() {
+  createCanvas(575,500);
+  background(96);
+  noFill();
+  angleMode(DEGREES);
+}
+
+function draw() {
+
+//daria equal triangle
+push();
+stroke(255, 0, 0);
+strokeWeight(3);
+  darRightTri(100, 75, 100, 0);
+  point(100,75);
+fill(255);
+  darRightTri(225, 75, 100, 0);
+  point(225,75);
+pop();
+//lyndon equal triangle
+push();
+stroke(0, 0, 255);
+strokeWeight(3);
+  lynTriEqual(350, 100, 100, 0);
+  point(350,100);
+fill(255);
+  lynTriEqual(475, 100, 100, 0);
+  point(475,100);
+pop();
+//lyndon right triangle
+push();
+stroke(0, 0, 255);
+strokeWeight(3);
+  lynTriRight(350, 200, 1, 1, 100, 0);
+  point(350,200);
+fill(255);
+  lynTriRight(475, 200, 1, 1, 100, 0);
+  point(475,200);
+pop();
+//lyndon isosceles triangle
+push();
+stroke(0, 0, 255);
+strokeWeight(3);
+  lynTriIsosceles(350, 300, 1, 1, 100, 0);
+  point(350,300);
+fill(255);
+  lynTriIsosceles(475, 300, 1, 1, 100, 0);
+  point(475,300);
+pop();
+
+//lyndon hexagon
+push();
+ strokeWeight(.5);
+ stroke(239,13,103);
+  lynHex(150, 250, 100, 0);
+ stroke(255, 0, 0);
+  lynHex(150, 250, 100, 15);
+ stroke(0, 255, 0);
+  lynHex(150, 250, 100, 30);
+ stroke(0, 0, 255);
+  lynHex(150, 250, 100, 45);
+ pop();
+}
+
+function darRightTri(lx, ly, sc, r){
+  let x1 = -(sc * 0.5);
+  let x2 = sc * 0.5;
+  let x3 = 0;
+  let y1 = -(sc * Math.sqrt(0.75) / 3);
+  let y2 = -(sc * Math.sqrt(0.75) / 3)
+  let y3 = sc / Math.sqrt(3);
+
+  push();
+  translate(lx, ly);
+  rotate(r);
+  triangle(x1, y1, x2, y2, x3, y3);
+  pop();
+}
+
+//equal
+function lynTriEqual(lx, ly, sc, r){
+  //let side = sc;
+  let s2 = sc/2;
+  let long = sc / Math.sqrt(3);
+  let small = (Math.sqrt(0.75)/3) * sc;
+
+  let x1 = -s2;
+  let x2 = 0;
+  let x3 = s2;
+  let y1 = small;
+  let y2 = -long;
+  let y3 = small;
+
+  push();
+   translate(lx, ly);
+   rotate(r);
+    triangle(x1, y1, x2, y2, x3, y3);
+  pop();
+}
+//right
+function lynTriRight(lx, ly, s1, s2, sc, r){
+
+  let s = (Math.sqrt(0.75)/3)
+  let l = s * 2
+
+  let x1 = -l*sc*s1
+  let x2 = s*sc
+  let x3 = s*sc
+  let y1 = s*sc*s2
+  let y2 = s*sc*s2
+  let y3 = -l*sc*s2
+
+  push();
+   translate(lx, ly);
+   rotate(r);
+    triangle(x1, x2, x3, y1, y2, y3);
+  pop();
+}
+//isosceles 
+function lynTriIsosceles(lx, ly, s1, s2, sc, r){
+
+  let s = (Math.sqrt(0.75)/3)
+  let l = s * 2
+
+  let x1 = -s1/2*sc
+  let x2 = s1/2*sc
+  let x3 = 0
+  let y1 = s*sc
+  let y3 = -s2*l*sc
+
+  push();
+   translate(lx, ly);
+   rotate(r);
+    triangle(x1, y1, x2, y1, x3, y3)
+  pop();
+}
+
+//hexagon
+function lynHex(lx, ly, sc, r){
+
+  let y1 = Math.sqrt(0.75)*sc;
+  
+  push();
+   translate(lx, ly);
+   rotate(r);
+    line(-sc/2, -y1, sc/2, -y1);
+    line(sc/2, -y1, sc, 0);
+    line(sc, 0, sc/2, y1);
+    line(sc/2, y1, -sc/2, y1);
+    line(-sc/2, y1, -sc, 0);
+    line(-sc, 0, -sc/2, -y1);
+  pop();   
+
+  let b1 = (y1/3);
+  let f = (255,64,128);
+
+push();
+ translate(lx, ly);
+ rotate(r); 
+  fill(f);
+noStroke();
+lynTriEqual( 0, y1-b1, 100, 0);
+lynTriEqual( -sc/2, -b1, 100, 0);
+lynTriEqual( sc/2, -b1, 100, 0);
+
+lynTriEqual( 0, -y1+b1, 100, 180);
+lynTriEqual( -sc/2, b1, 100, 180);
+lynTriEqual( sc/2, b1, 100, 180);
+pop();
+
+
+}
